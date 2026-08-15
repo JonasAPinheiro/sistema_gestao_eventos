69ea112b45970a56f1e6458759d49165bf5818a3
Jonas Pinheiro <jonas.pinheiro@aluno.ifsp.edu.br>
2026-08-15 18:22:14 -0300

    feat: Criação do projeto

---
+++ src/main/java/br/com/sge/sistemagestaoeventos/SistemaGestaoEventosApplication.java
@@ -0,0 +1,13 @@
+package br.com.sge.sistemagestaoeventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestaoEventosApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Projeto Criado");
+        SpringApplication.run(SistemaGestaoEventosApplication.class, args);
+    }
+}
---
+++ src/test/java/br/com/sge/sistemagestaoeventos/SistemaGestaoEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sge.sistemagestaoeventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestaoEventosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
