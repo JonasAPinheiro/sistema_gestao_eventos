68f6bd4bcc4c4fc5393dffb7db57e3ecfa85e200
Jonas Pinheiro <jonas.pinheiro@aluno.ifsp.edu.br>
2026-08-16 17:02:51 -0300

    feat: Adicionando a bean repository no EventoRepositoryMemoria

--- src/main/java/br/com/sge/sistemagestaoeventos/repository/EventoRepositoryMemoria.java
+++ src/main/java/br/com/sge/sistemagestaoeventos/repository/EventoRepositoryMemoria.java
@@ -1,12 +1,14 @@
 package br.com.sge.sistemagestaoeventos.repository;
 
 import br.com.sge.sistemagestaoeventos.model.Evento;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Repository
 public class EventoRepositoryMemoria implements EventoRepository{
     private final Map<String, Evento> dados = new ConcurrentHashMap<>();
 
