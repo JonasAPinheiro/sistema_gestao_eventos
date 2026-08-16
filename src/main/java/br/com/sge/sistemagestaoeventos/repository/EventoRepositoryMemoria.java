4220681142e6706c91c365a4d20dde47e47f1ee7
Jonas Pinheiro <jonas.pinheiro@aluno.ifsp.edu.br>
2026-08-16 16:17:13 -0300

    feat: Criação do repository de evento

---
+++ src/main/java/br/com/sge/sistemagestaoeventos/repository/EventoRepositoryMemoria.java
@@ -0,0 +1,33 @@
+package br.com.sge.sistemagestaoeventos.repository;
+
+import br.com.sge.sistemagestaoeventos.model.Evento;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class EventoRepositoryMemoria implements EventoRepository{
+    private final Map<String, Evento> dados = new ConcurrentHashMap<>();
+
+    @Override
+    public Optional<Evento> buscarPorId(String id) {
+        return Optional.ofNullable(dados.get(id));
+    }
+
+    @Override
+    public List<Evento> listarTodos() {
+        return List.copyOf(dados.values());
+    }
+
+    @Override
+    public boolean existePorId(String id) {
+        return dados.containsKey(id);
+    }
+
+    @Override
+    public Evento salvar(Evento evento) {
+        dados.put(evento.getId(), evento);
+        return evento;
+    }
+}
