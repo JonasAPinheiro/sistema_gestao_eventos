63b4aad76ecfa520dc0d71675cb69e329de6a38a
Jonas Pinheiro <jonas.pinheiro@aluno.ifsp.edu.br>
2026-09-20 18:41:53 -0300

    feat: Removendo log desnecessário no SistemaGestaoEventosApplication

--- src/main/java/br/com/sge/sistemagestaoeventos/SistemaGestaoEventosApplication.java
+++ src/main/java/br/com/sge/sistemagestaoeventos/SistemaGestaoEventosApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SistemaGestaoEventosApplication {
 
     public static void main(String[] args) {
-        System.out.println("Projeto Criado");
         SpringApplication.run(SistemaGestaoEventosApplication.class, args);
     }
 }
