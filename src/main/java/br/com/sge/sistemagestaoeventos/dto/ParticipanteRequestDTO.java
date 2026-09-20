43d6fb67c514289c982605f531ac6547eb239ce9
Ronaldo-Junior1 <ronaldosjr2005@gmail.com>
2026-09-20 19:23:46 -0300

    refactor: adiciona conversão do DTO para entidade

--- src/main/java/br/com/sge/sistemagestaoeventos/dto/ParticipanteRequestDTO.java
+++ src/main/java/br/com/sge/sistemagestaoeventos/dto/ParticipanteRequestDTO.java
@@ -1,6 +1,12 @@
 package br.com.sge.sistemagestaoeventos.dto;
 
+import br.com.sge.sistemagestaoeventos.model.Participante;
+
 public record ParticipanteRequestDTO(
         String nome,
         String email
-) {}
+) {
+    public Participante toParticipante() {
+        return new Participante(nome, email);
+    }
+}
